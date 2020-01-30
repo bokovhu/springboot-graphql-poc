7772968237ea67afa8ed3bdb29e48ca920439c90
Botond János Kovács <botondjanoskovacs@gmail.com>
2020-01-30 17:40:42 +0100

    Initial commit

---
+++ src/main/java/me/bokov/springboot/graphql/SpringbootGraphqlPocApplication.java
@@ -0,0 +1,13 @@
+package me.bokov.springboot.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlPocApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run (SpringbootGraphqlPocApplication.class, args);
+    }
+
+}
